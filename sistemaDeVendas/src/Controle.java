c3e5a41bdf496dcfff2a7c341195654c95682226
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2024-11-26 21:27:30 -0300

    (tentativa) de implantação de método de mudança de status de venda

--- sistemaDeVendas/src/Controle.java
+++ sistemaDeVendas/src/Controle.java
@@ -3,11 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Controle {
+    Scanner sc = new Scanner(System.in);
     private List<Venda> vendas = new ArrayList<>();
     private List<Produto> produtos = new ArrayList<>();
     private List<Cliente> clientes = new ArrayList<>();
-
-    Scanner sc = new Scanner(System.in);
 
     public void criarVenda() {
         //TODO -> implementar lógica
@@ -18,19 +17,30 @@ public class Controle {
         for (Venda venda : vendas) {
             if (!venda.getId().equals(idVenda)) {
                 System.err.println("Não existe venda com esse ID.");
+                break;
             }
+            System.out.println("Qual o novo status da venda " + idVenda + "?");
+            System.out.println("(1 = Reserva, 2 = Pago Total, 3 = Enviado)");
+            int novoStatus = sc.nextInt();
+
+            switch (novoStatus) {
+                case 1:
+                    //TODO -> Falta implementar a lógica para encontrar a venda pelo ID na lista
+                    break;
+                case 2:
+                    //TODO -> Falta implementar a lógica para encontrar a venda pelo ID na lista
+                    break;
+                case 3:
+                    //TODO -> Falta implementar a lógica para encontrar a venda pelo ID na lista
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+
         }
 
-        System.out.println("Qual o novo status da venda " + idVenda + "?");
-        String status = sc.nextLine();
 
-        if (status.equalsIgnoreCase("reserva")) {
-
-        } else if (status.equalsIgnoreCase("pagototal")) {
-
-        } else {
-
-        }
 
     }
 
